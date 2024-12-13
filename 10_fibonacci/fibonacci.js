a3c64f45f8157eb1b810b03c124bd1096bb82873
Jeffy Pun <167040139+Jeffy46@users.noreply.github.com>
2024-12-13 03:14:23 -0500

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    number = parseInt(num);
+    if(number==0){
+        return 0;
+    }
+    if(number < 0){
+        return "OOPS"
+    }
+    let first=1;
+    let second=1;
+    for(let i=1; i < number;i++){
+        temp = second;
+        second=first+second;
+        first=temp;
+    }
+    return first;
 };
 
 // Do not edit below this line
