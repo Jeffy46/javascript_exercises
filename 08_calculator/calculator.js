21f72a67f442d363e1186caa631312cdc303d5b4
Jeffy Pun <167040139+Jeffy46@users.noreply.github.com>
2024-12-13 00:17:40 -0500

    completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbersToAdd) {
+	const arr = numbersToAdd;
+  let sum=0;
+  for(num of arr){
+    sum+=num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbersToMultiply) {
+  const arr=numbersToMultiply;
+  let product=1;
+  for(num of arr){
+    product*=num;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return Math.pow(num1,num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let product=1;
+  for(let i=num;i>=1;i--){
+    product*=i;
+  }
+  return product;
 };
 
 // Do not edit below this line
