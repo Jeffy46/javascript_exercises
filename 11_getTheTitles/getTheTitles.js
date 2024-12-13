9f17c030fcf1b8d4e43b8d264af3fd1d1e825840
Jeffy Pun <167040139+Jeffy46@users.noreply.github.com>
2024-12-13 03:17:21 -0500

    completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const ans=[];
+    for(item of array){
+        ans.push(item.title);
+    }
+    return ans;
 };
 
 // Do not edit below this line
