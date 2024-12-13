1736e4d37b4a9b8c5b99216abb5dc4b34b0f7b22
Jeffy Pun <167040139+Jeffy46@users.noreply.github.com>
2024-12-12 23:50:47 -0500

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let ans=0;
+    if(Number.isInteger(num1) && Number.isInteger(num2) && num1+num2>0){
+        let lowRange, highRange;
+        if(num1<num2){
+            lowRange=num1;
+            highRange=num2;
+        }else{
+            lowRange=num2;
+            highRange=num1;
+        }
+        for(let i=lowRange;i<=highRange;i++){
+            ans+=i;
+        }
+    }else{
+        return "ERROR";
+    }
+    return ans;
 };
 
 // Do not edit below this line
