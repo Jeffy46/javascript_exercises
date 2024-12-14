b12f0e76bf8461d9fb2ddad4aa528740c0dcc148
Jeffy Pun <167040139+Jeffy46@users.noreply.github.com>
2024-12-13 21:29:08 -0500

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let biggestDif = 0;
+    let oldestPersonIndex;
+    for(let i=0;i<array.length;i++){
+        if(array[i].yearOfDeath==null){
+            let d = new Date();
+            array[i].yearOfDeath=d.getFullYear()
+        }
+        let dif=array[i].yearOfDeath-array[i].yearOfBirth;
+            if(dif>biggestDif){
+                oldestPersonIndex=i;
+                biggestDif=dif;
+            }
+    }
+    return array[oldestPersonIndex];
 };
 
 // Do not edit below this line
